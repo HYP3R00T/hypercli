73792fde668411cc1adacad3548b0ccdc85d0f80
the-hyperoot <rajesh.kumar.das.tech@gmail.com>
2022-12-31 03:07:13 +0530

    minor modification

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ with open("./README.md", "r") as readme:
 
 setuptools.setup(
     name="hypercli",
-    version="0.1.3",
+    version="0.1.4",
     author="HYP3R00T",
     author_email="contact@hyperoot.live",
     description="A python based CLI tool",
     url="https://github.com/the-hyperoot/hyper_cli",
+    long_description=desc,
+    long_description_content_type='text/markdown',
     license='MIT',
     packages=['hypercli'],
     zip_safe=False,
