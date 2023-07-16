0d09a9110d2df5e209f42a14a8c5d07f582d7e00
the-hyperoot <rajesh.kumar.das.tech@gmail.com>
2023-07-17 00:52:00 +0530

    updated license and email

--- setup.py
+++ setup.py
@@ -7,16 +7,13 @@ setuptools.setup(
     name="hypercli",
     version="0.2.0",
     author="HYP3R00T",
-    author_email="contact@hyperoot.live",
+    author_email="hyperoot@pm.me",
     description="Enhanced Menu and CLI generator",
     url="https://github.com/the-hyperoot/hyper_cli",
     long_description=desc,
-    long_description_content_type='text/markdown',
-    license='MIT',
-    packages=['hypercli'],
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=["hypercli"],
     zip_safe=False,
-    install_requires=['pyfiglet',
-                      'rich',
-                      'termcolor'
-                      ]
+    install_requires=["pyfiglet", "rich", "termcolor"],
 )
